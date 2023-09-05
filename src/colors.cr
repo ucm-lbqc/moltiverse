3944735988cf6ddfb0d09454d970476ec6d3caf6
Mauricio Bedoya <maurobedoyat@gmail.com>
2023-09-04 21:34:28 -0300

    Create colors.cr
    This module is to make prettier the console messages.

---
+++ src/colors.cr
@@ -0,0 +1,9 @@
+module Coloring
+    AQUA = Colorize::ColorRGB.new(0, 255, 255)
+    ELECTRIC_BLUE = Colorize::ColorRGB.new(30, 144, 255)
+    PURPLE = Colorize::ColorRGB.new(128, 0, 128)
+    TURQUOISE = Colorize::ColorRGB.new(64, 224, 208)
+    GREEN = Colorize::ColorRGB.new(0, 163, 131)
+    RED = Colorize::ColorRGB.new(214, 69, 80)
+    YELLOW = Colorize::ColorRGB.new(255, 191, 0)
+end
