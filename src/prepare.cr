7ae69452abd1b587677cd16a6822d8d8d897f8fd
Mauricio Bedoya <maurobedoyat@gmail.com>
2023-09-04 21:32:06 -0300

    Add parameterization function
    This function requires python in the background. For now is experimental and would drastically change in the future.

--- src/prepare.cr
+++ src/prepare.cr
@@ -150,3 +150,175 @@ module Prepare
       #  exit
       #end
     end
+    def parameterize
+      outfile = "rdkit_leap.py"
+      # Convert "water" variable to yes or no for python.
+      water = @explicit_water ? "yes" : "no"
+      basename = @basename
+      jump = "\\n"
+      File.write outfile, <<-SCRIPT
+      from rdkit import Chem
+      from rdkit.Chem import AllChem
+      import parmed as pmd
+      import subprocess
+      import os
+      import shutil
+      if "#{@seed}" == "no":
+        seed = False
+      else:
+        seed = int("#{@seed}")
+      water = "#{water}"
+    
+      def run_silent(command, basename):
+        with open(os.devnull, 'w')  as FNULL:
+                try:
+                    subprocess.run(command, shell=True, stdout=FNULL)
+                except subprocess.CalledProcessError:
+                    print(f"Error occured with system {basename}")
+    
+      #starting = Chem.rdmolfiles.MolFromMol2File(
+      #          f"#{basename}.mol2", sanitize=True, removeHs=False)
+      starting = Chem.rdmolfiles.MolFromPDBFile(
+                f"#{basename}.pdb", sanitize=True, removeHs=False)
+      # Write properties
+      natoms = Chem.rdchem.Mol.GetNumAtoms(starting)
+      nrot = Chem.rdMolDescriptors.CalcNumRotatableBonds(starting)
+      nbonds = starting.GetNumBonds()
+      charge = Chem.GetFormalCharge(starting)
+      print("Ligand structure:", "#{basename}.pdb")
+      print("Atoms : ", natoms)
+      print("Bonds : ", nbonds)
+      print("Rbonds: ", nrot)
+      print("Charge: ", charge)
+      if seed:
+        # Generating random conformer from smiles:
+        print("Generating random conformer from smiles code:")
+        smiles = Chem.MolToSmiles(starting, allHsExplicit=True)
+        print(smiles)
+        ps = Chem.SmilesParserParams()
+        ps.removeHs = False
+        new = Chem.MolFromSmiles(smiles,ps)
+        AllChem.EmbedMolecule(new, randomSeed=seed)
+        print("Data generated from smiles code:")
+        smiles_natoms = Chem.rdchem.Mol.GetNumAtoms(new)
+        smiles_nrot = Chem.rdMolDescriptors.CalcNumRotatableBonds(new)
+        smiles_nbonds = new.GetNumBonds()
+        smiles_charge = Chem.GetFormalCharge(new)
+        print("Seed : ", seed)
+        print("Atoms : ", smiles_natoms)
+        print("Bonds : ", smiles_nbonds)
+        print("Rbonds: ", smiles_nrot)
+        print("Charge: ", smiles_charge)
+        if natoms != smiles_natoms:
+           print("Number of atoms differs between the original structure and the random structure generated with RDkit. Check your structure.")
+           exit(1)
+        elif nbonds != smiles_nbonds:
+          print("Number of bonds differs between the original structure and the random structure generated with RDkit. Check your structure.")
+          exit(1)
+        elif nrot != smiles_nrot:
+          print("Number of rotable bonds differs between the original structure and the random structure generated with RDkit. Check your structure.")
+          exit(1)
+        elif charge != smiles_charge:
+          print("Formal charge differs between the original structure and the random structure generated with RDkit. Check your structure.")
+          exit(1)
+        Chem.rdmolfiles.MolToPDBFile(new, "#{basename}.pdb")
+        print("Successful random generation.")
+      else:
+        Chem.rdmolfiles.MolToPDBFile(starting, "#{basename}.pdb")
+        print("Original coordinates were preserved.")
+      # Connectivity info must be erased previous to leap, if not, it will fail.
+      # TO:DO Don´t write connectivity info (use chem.cr)
+      with open("#{basename}.pdb", "r") as input:
+        with open("temp.txt", "w") as output:
+          # iterate all lines from file
+          for line in input:
+            if not line.strip("\\n").startswith('CONECT'):
+              output.write(line)
+      # replace file with original name
+      shutil.copy('temp.txt', '#{basename}.pdb')
+      #print("Running leap...")
+      with open('run_leap.sh', 'w') as f:
+          f.write(f'ligand="#{basename[0...3]}"' + '#{jump}')
+          f.write(f'pdb="#{basename}"' + '#{jump}')
+          f.write(f'charge="{charge}"' + '#{jump}')
+          f.write(
+              f'antechamber -i $pdb.pdb -fi pdb -o $ligand.mol2 -fo mol2 -c bcc -nc $charge -rn $ligand -at gaff2' + '#{jump}')
+          f.write(
+              f'parmchk2 -i $ligand.mol2 -f mol2 -o $ligand.frcmod -s gaff2' + '#{jump}')
+          f.write('cat > tleap1.in <<- EOS' + '#{jump}')
+          f.write('source leaprc.gaff2' + '#{jump}')
+          f.write('LIG = loadmol2 ${ligand}.mol2' + '#{jump}')
+          f.write('loadamberparams ${ligand}.frcmod' + '#{jump}')
+          f.write('saveAmberParm LIG ${pdb}.prmtop ${pdb}.inpcrd' + '#{jump}')
+          f.write('savePdb LIG ${pdb}.pdb' + '#{jump}')
+          f.write('quit' + '#{jump}')
+          f.write('EOS' + '#{jump}')
+          f.write('tleap -s -f tleap1.in > ${ligand}_tleap1.out' + '#{jump}')
+          if water == "yes":
+            f.write('cat > tleap2.in <<- EOS' + '#{jump}')
+            f.write('source leaprc.gaff2' + '#{jump}')
+            f.write('source leaprc.water.tip3p' + '#{jump}')
+            f.write('LIG = loadmol2 ${ligand}.mol2' + '#{jump}')
+            f.write('solvatebox LIG SPCBOX 20 iso' + '#{jump}')
+            f.write('loadamberparams ${ligand}.frcmod' + '#{jump}')
+            f.write('saveAmberParm LIG ${pdb}_solv.prmtop ${pdb}_solv.inpcrd' + '#{jump}')
+            f.write('savePdb LIG ${pdb}_solv.pdb' + '#{jump}')
+            f.write('quit' + '#{jump}')
+            f.write('EOS' + '#{jump}')
+            f.write('tleap -s -f tleap2.in > ${ligand}_tleap2.out' + '#{jump}')
+      os.chmod("run_leap.sh", 0o777)
+      cmd = f"./run_leap.sh"
+      run_silent(cmd, "Running Leap")
+      #print("Running ParmEd...")
+      # Parmed format file conversion single ligand
+      amber_ligand = pmd.load_file('#{basename}.prmtop', xyz='#{basename}.inpcrd')
+      amber_ligand.save('#{basename}.psf', overwrite=True)
+      amber_ligand.save('#{basename}.top', overwrite=True)
+      amber_ligand.save('#{basename}.gro', overwrite=True)
+      amber_ligand.save('#{basename}.pqr', overwrite=True)
+      
+      SCRIPT
+      parmed_exec = "python"
+      arguments = ["rdkit_leap.py"]
+      puts "Parameterizing ligand with tleap ..."
+      run_cmd(cmd=parmed_exec, args=arguments, output_file=Nil, stage="parameterization", verbose=true)
+      # TO:DO Check if the antechamber - leap process was successful.
+      if water == "no"
+        top_file = "#{@basename}.prmtop"
+        coord_file = "#{@basename}.inpcrd"
+        if File.exists?(top_file)
+          @topology_file = Path.new(top_file).expand().to_s
+        else
+          puts "Topology file was not generated. Check the *.out log files."
+          exit
+        end
+        if File.exists?(coord_file)
+          @coordinates_file = Path.new(coord_file).expand().to_s
+        else
+          puts "Coordinates file was not generated. Check the *.out log files."
+          exit
+        end
+        @basename = "#{@basename}"
+        @pdb_system = "#{@basename}.pdb"
+        puts "SYSTEM INFO: ".colorize(PURPLE), Chem::Structure.from_pdb(@pdb_system)
+      end
+      if water == "yes"
+        top_file = "#{@basename}_solv.prmtop"
+        coord_file = "#{@basename}_solv.inpcrd"
+        @pdb_system = "#{@basename}_solv.pdb"
+        if File.exists?(top_file)
+          @topology_file = Path.new(top_file).expand().to_s
+        else
+          puts "Topology file was not generated. Check the *.out log files."
+          exit
+        end
+        if File.exists?(coord_file)
+          @coordinates_file = Path.new(coord_file).expand().to_s
+        else
+          puts "Coordinates file was not generated. Check the *.out log files."
+          exit
+        end
+        @basename = "#{@basename}_solv"
+        puts "SYSTEM INFO: ", Chem::Structure.from_pdb(pdb_system)
+      end
+    end
