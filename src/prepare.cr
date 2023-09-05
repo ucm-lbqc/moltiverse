7c2ac9e63a2914ac431bdb46c3c25bf20a05d922
Mauricio Bedoya <maurobedoyat@gmail.com>
2023-09-04 21:31:07 -0300

    Add add_h function

--- src/prepare.cr
+++ src/prepare.cr
@@ -100,3 +100,53 @@ module Prepare
     def lig_center
       @lig_center
     end
+    def add_h
+      puts "The output folder name will be: #{@output_name}"
+      if Dir.exists?("#{@output_name}")
+        Dir.cd(@output_name)
+      else
+        puts "Creating folder #{@output_name}"
+        Dir.mkdir(@output_name)
+        Dir.cd(@output_name)
+      end
+      # 1. This stage cheks if hydrogens must be preserved, if so, only convert the file to .pdb using openbabel.
+      # This must be done because RDKit does not read the formal charge  correctly when, possibly, connectivities and
+      # charge in the last column of atoms do not specify the atom partial charges.
+      # TO:DO 1. Proper conversion to read adecuately the formal charge. Extremely important for tleap parameterization.
+      # TO:DO 2. Test antechamber to use as input the mol2 file. Require a formal charge especification?
+      # TO:DO 3. If keep_hydrogens == yes and input file is a PDB, ask the user to specify the formal charge, and use it for
+      # tleap parameterization. 
+      if @keep_hydrogens
+        obabel = "obabel"
+        args1 = ["-i", "#{@format}", "#{@file}", "-O", "#{@basename}.pdb",]
+        puts "Running openbabel convertion..."
+        run_cmd(cmd=obabel, args=args1, output_file=Nil, stage="converting to pdb with connectivities", verbose=true)
+        puts "Converted"
+        @basename = "#{@basename}"
+        @format = "pdb"
+        @extension = ".pdb"
+        File.copy("#{basename}#{extension}", "original_keep_hydrogens#{extension}")
+      else
+        obabel = "obabel"
+        args1 = ["-i", "#{@format}", "#{@file}", "-O", "#{@basename}_h.pdb", "-p", "#{@ph}"]
+        puts "Running openbabel convertion..."
+        run_cmd(cmd=obabel, args=args1, output_file=Nil, stage="hydrogen addition", verbose=true)
+        puts "Converted"
+        @basename = "#{@basename}_h"
+        @format = "pdb"
+        @extension = ".pdb"
+        File.copy("#{@basename}.pdb", "original_no-keep_hydrogens#{extension}")
+      end
+
+      new_file = "#{@basename}#{@extension}"
+      @file = Path.new(new_file).expand().to_s
+      @path = Path.new(new_file).expand().parent()
+      # TO:DO Add a proper convertion from other formats. mol, mol2, sdf, when the --keep_hydrogens = true options is used.
+      #begin
+      #  @charge = Chem::Structure.from_pdb(@file).formal_charge()
+      #  puts "charge chem.cr: #{@charge}"
+      #rescue exception
+      #    puts "File #{@file} could not be read. If the option --keep_hydrogens = true, make sure that the original file is a PDB.".colorize(RED)
+      #  exit
+      #end
+    end
