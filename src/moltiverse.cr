9cfeb6059518d1cd910f8d273ba749420045fad1
Mauricio Bedoya <maurobedoyat@gmail.com>
2023-09-04 21:07:30 -0300

    Add parser function and input ligand file option.
    Input formats are not completely defined yet

--- src/moltiverse.cr
+++ src/moltiverse.cr
@@ -32,3 +32,12 @@ bounds_colvars = BoundsColvars.new(0.0, 10.0, 20, 2, 0, 0, 0, 0)
 dimension = 1
 metadynamics = false
 
+OptionParser.parse do |parser|
+  parser.banner = "Usage: crystal moltiverse.cr [OPTIONS]"
+  parser.on("-l FILE", "--ligand=FILE", "Input ligand file [PDB, MOL or MOL2]") do |str|
+    unless File.exists?(str)
+      STDERR.puts "Error: ligand file not found: #{str}"
+      exit(1)
+    end
+    ligand = str
+  end
