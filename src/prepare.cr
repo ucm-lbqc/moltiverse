5023fa979ec5396ede6b8d6fb07b7c4f789ad649
Mauricio Bedoya <maurobedoyat@gmail.com>
2023-09-04 21:30:14 -0300

    Create prepare.cr

---
+++ src/prepare.cr
@@ -0,0 +1,16 @@
+require "./namdconf.cr"
+require "ecr"
+require "chem"
+require "./protocols.cr"
+require "./analysis.cr"
+require "colorize"
+require "./colors.cr"
+require "./execution.cr"
+
+include Namdconf
+include Chem
+include Protocols
+include Analysis
+include Coloring
+include Execution
+
