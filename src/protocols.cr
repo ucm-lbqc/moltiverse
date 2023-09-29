d173c2d82fcc32ae5477d493712b7d18ad4f3316
Mauricio Bedoya <maurobedoyat@gmail.com>
2023-09-29 12:37:49 -0300

    Refactor time

--- src/protocols.cr
+++ src/protocols.cr
@@ -277,7 +277,7 @@ module Protocols
               lw_rdgyr = pair_rdgyr[0]
               up_rdgyr = pair_rdgyr[1] 
               # Writting namd configuration
-              enhanced_sampling(lig.explicit_water, lig.basename, lig.topology_file, lig.coordinates_file, "#{type}.#{window}.namd", @time_rmsd, @time_rdgyr, window, type).to_s
+              enhanced_sampling(lig.explicit_water, lig.basename, lig.topology_file, lig.coordinates_file, "#{type}.#{window}.namd", @time_rmsd, window, type).to_s
               # Writting colvars configuration
               colvars(@metadynamics, 
               lw_rmsd, 
