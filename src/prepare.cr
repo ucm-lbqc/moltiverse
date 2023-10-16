96689e64760d344fa7c3eff642dd75a79280ac25
Mauricio Bedoya <maurobedoyat@gmail.com>
2023-10-16 19:50:30 +0200

    Update parameterize method
    This is a drastic change to use openbabel instead of rdkit to prepare the ligand and to generate the input files for the antechamber and parmchk2 executables to parameterize the molecule.

--- src/prepare.cr
+++ src/prepare.cr
@@ -200,141 +200,40 @@ module Prepare
     end
 
     def parameterize
-      outfile = "rdkit_leap.py"
-      # Convert "water" variable to yes or no for python.
-      water = @explicit_water ? "yes" : "no"
-      basename = @basename
-      jump = "\\n"
-      File.write outfile, <<-SCRIPT
-      from rdkit import Chem
-      from rdkit.Chem import AllChem
-      #import parmed as pmd
-      import subprocess
-      import os
-      import shutil
-      if "#{@random_coords}" == "no":
-        random_coords = False
-      else:
-        random_coords = int("#{@random_coords}")
-      water = "#{water}"
-    
-      def run_silent(command, basename):
-        with open(os.devnull, 'w')  as FNULL:
-                try:
-                    subprocess.run(command, shell=True, stdout=FNULL)
-                except subprocess.CalledProcessError:
-                    print(f"Error occured with system {basename}")
-    
-      #starting = Chem.rdmolfiles.MolFromMol2File(
-      #          f"#{basename}.mol2", sanitize=True, removeHs=False)
-      starting = Chem.rdmolfiles.MolFromPDBFile(
-                f"#{basename}.pdb", sanitize=True, removeHs=False)
-      # Write properties
-      natoms = Chem.rdchem.Mol.GetNumAtoms(starting)
-      nrot = Chem.rdMolDescriptors.CalcNumRotatableBonds(starting)
-      nbonds = starting.GetNumBonds()
-      charge = Chem.GetFormalCharge(starting)
-      print("Ligand structure:", "#{basename}.pdb")
-      print("Atoms : ", natoms)
-      print("Bonds : ", nbonds)
-      print("Rbonds: ", nrot)
-      print("Charge: ", charge)
-      if random_coords:
-        # Generating random conformer from smiles:
-        print("Generating random conformer from smiles code:")
-        smiles = Chem.MolToSmiles(starting, allHsExplicit=True)
-        print(smiles)
-        ps = Chem.SmilesParserParams()
-        ps.removeHs = False
-        new = Chem.MolFromSmiles(smiles,ps)
-        AllChem.EmbedMolecule(new, Seed=random_coords)
-        print("Data generated from smiles code:")
-        smiles_natoms = Chem.rdchem.Mol.GetNumAtoms(new)
-        smiles_nrot = Chem.rdMolDescriptors.CalcNumRotatableBonds(new)
-        smiles_nbonds = new.GetNumBonds()
-        smiles_charge = Chem.GetFormalCharge(new)
-        print("Random coords : ", random_coords)
-        print("Atoms : ", smiles_natoms)
-        print("Bonds : ", smiles_nbonds)
-        print("Rbonds: ", smiles_nrot)
-        print("Charge: ", smiles_charge)
-        if natoms != smiles_natoms:
-           print("Number of atoms differs between the original structure and the random structure generated with RDkit. Check your structure.")
-           exit(1)
-        elif nbonds != smiles_nbonds:
-          print("Number of bonds differs between the original structure and the random structure generated with RDkit. Check your structure.")
-          exit(1)
-        elif nrot != smiles_nrot:
-          print("Number of rotable bonds differs between the original structure and the random structure generated with RDkit. Check your structure.")
-          exit(1)
-        elif charge != smiles_charge:
-          print("Formal charge differs between the original structure and the random structure generated with RDkit. Check your structure.")
-          exit(1)
-        Chem.rdmolfiles.MolToPDBFile(new, "#{basename}.pdb")
-        print("Successful random generation.")
-      else:
-        Chem.rdmolfiles.MolToPDBFile(starting, "#{basename}.pdb")
-        print("Original coordinates were preserved.")
-      # Connectivity info must be erased previous to leap, if not, it will fail.
-      # TO:DO Don´t write connectivity info (use chem.cr)
-      with open("#{basename}.pdb", "r") as input:
-        with open("temp.txt", "w") as output:
-          # iterate all lines from file
-          for line in input:
-            if not line.strip("\\n").startswith('CONECT'):
-              output.write(line)
-      # replace file with original name
-      shutil.copy('temp.txt', '#{basename}.pdb')
-      #print("Running leap...")
-      with open('run_leap.sh', 'w') as f:
-          f.write(f'ligand="#{basename[0...3]}"' + '#{jump}')
-          f.write(f'pdb="#{basename}"' + '#{jump}')
-          f.write(f'charge="{charge}"' + '#{jump}')
-          f.write(
-              f'antechamber -i $pdb.pdb -fi pdb -o $ligand.mol2 -fo mol2 -c bcc -nc $charge -rn $ligand -at gaff2' + '#{jump}')
-          f.write(
-              f'parmchk2 -i $ligand.mol2 -f mol2 -o $ligand.frcmod -s gaff2' + '#{jump}')
-          f.write('cat > tleap1.in <<- EOS' + '#{jump}')
-          f.write('source leaprc.gaff2' + '#{jump}')
-          f.write('LIG = loadmol2 ${ligand}.mol2' + '#{jump}')
-          f.write('loadamberparams ${ligand}.frcmod' + '#{jump}')
-          f.write('saveAmberParm LIG ${pdb}.prmtop ${pdb}.inpcrd' + '#{jump}')
-          f.write('savePdb LIG ${pdb}.pdb' + '#{jump}')
-          f.write('quit' + '#{jump}')
-          f.write('EOS' + '#{jump}')
-          f.write('tleap -s -f tleap1.in > ${ligand}_tleap1.out' + '#{jump}')
-          if water == "yes":
-            f.write('cat > tleap2.in <<- EOS' + '#{jump}')
-            f.write('source leaprc.gaff2' + '#{jump}')
-            f.write('source leaprc.water.tip3p' + '#{jump}')
-            f.write('LIG = loadmol2 ${ligand}.mol2' + '#{jump}')
-            f.write('solvatebox LIG SPCBOX 20 iso' + '#{jump}')
-            f.write('loadamberparams ${ligand}.frcmod' + '#{jump}')
-            f.write('saveAmberParm LIG ${pdb}_solv.prmtop ${pdb}_solv.inpcrd' + '#{jump}')
-            f.write('savePdb LIG ${pdb}_solv.pdb' + '#{jump}')
-            f.write('quit' + '#{jump}')
-            f.write('EOS' + '#{jump}')
-            f.write('tleap -s -f tleap2.in > ${ligand}_tleap2.out' + '#{jump}')
-      os.chmod("run_leap.sh", 0o777)
-      cmd = f"./run_leap.sh"
-      run_silent(cmd, "Running Leap")
-      #print("Running ParmEd...")
-      ## Parmed format file conversion single ligand
-      #amber_ligand = pmd.load_file('#{basename}.prmtop', xyz='#{basename}.inpcrd')
-      #amber_ligand.save('#{basename}.psf', overwrite=True)
-      #amber_ligand.save('#{basename}.top', overwrite=True)
-      #amber_ligand.save('#{basename}.gro', overwrite=True)
-      #amber_ligand.save('#{basename}.pqr', overwrite=True)
-      
-      SCRIPT
-      parmed_exec = "python"
-      arguments = ["rdkit_leap.py"]
+      # Convert to PDB after add_h and structure randomization and write PDB without connectivities
+      new_pdb = Chem::Structure.read("#{@basename}#{@extension}")
+      new_pdb.to_pdb("#{@basename}.pdb", bonds: :none)
+      @extension = ".pdb"
+      @format = "pdb"
+      new_file = "#{@basename}#{@extension}"
+      @file = Path.new(new_file).expand.to_s
+      @path = Path.new(new_file).expand.parent
+      antechamber_exec = "antechamber"
+      arguments = ["-i", "#{file}", "-fi", "pdb", "-o", "#{basename}_prep.mol2", "-fo", "mol2", "-c", "bcc", "-nc", "#{@charge}", "-rn", "LIG", "-at", "gaff2"]
       puts "Parameterizing ligand with tleap ..."
-      run_cmd(cmd=parmed_exec, args=arguments, output_file=Nil, stage="parameterization", verbose=true)
-      # TO:DO Check if the antechamber - leap process was successful.
-      if water == "no"
-        top_file = "#{@basename}.prmtop"
-        coord_file = "#{@basename}.inpcrd"
+      run_cmd(cmd = antechamber_exec, args = arguments, output_file = Nil, stage = "Parameterization stage 1 ✔".colorize(GREEN), verbose = false)
+      parmchk2_exec = "parmchk2"
+      arguments = ["-i", "#{basename}_prep.mol2", "-f", "mol2", "-o", "#{basename}_prep.frcmod", "-s", "gaff2"]
+      run_cmd(cmd = parmchk2_exec, args = arguments, output_file = Nil, stage = "Parameterization stage 2 ✔".colorize(GREEN), verbose = false)
+
+      if @explicit_water
+        outfile = "tleap.in"
+        File.write outfile, <<-SCRIPT
+        source leaprc.gaff2
+        source leaprc.water.tip3p
+        LIG = loadmol2 "#{basename}_prep.mol2"
+        solvatebox LIG SPCBOX 20 iso
+        loadamberparams "#{basename}_prep.frcmod"
+        saveAmberParm LIG "#{basename}_prep_solv.prmtop" "#{basename}_prep_solv.inpcrd"
+        savePdb LIG "#{basename}_prep_solv.pdb"
+        quit
+        SCRIPT
+        tleap_exec = "tleap"
+        arguments = ["-s", "-f", "#{outfile}", ">", "#{basename}_tleap.out"]
+        run_cmd(cmd = tleap_exec, args = arguments, output_file = Nil, stage = "Parameterization stage 3 ✔".colorize(GREEN), verbose = false)
+        # Verify if topology and coordinates file were generated.
+        top_file = "#{@basename}_prep_solv.prmtop"
+        coord_file = "#{@basename}_prep_solv.inpcrd"
         if File.exists?(top_file)
           @topology_file = Path.new(top_file).expand.to_s
         else
