0d3854e8718158914aabd20870e1985d8d6bfac0
Mauricio Bedoya <maurobedoyat@gmail.com>
2023-11-20 18:30:33 +0100

    Fix analysis.cr
    This fix the n_frames function to get the number of dcd frames obtained.

--- src/analysis.cr
+++ src/analysis.cr
@@ -2,7 +2,7 @@ module Analysis
   def n_frames(pdb : String, dcd : String)
     structure = Chem::Structure.from_pdb(pdb)
     Chem::DCD::Reader.open((dcd), structure) do |reader|
-      n_frames = reader.n_entries - 1
+      n_frames = reader.n_entries
     end
   end
 end
