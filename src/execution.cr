b43ec0b1d544a6a182177ce6391a7ef250fca1c7
Mauricio Bedoya <maurobedoyat@gmail.com>
2023-11-20 18:32:03 +0100

    Update execution.cr
    This new function is useful to execute commands and does not print the output to console. Useful in the random conformer generated using openbabel.

--- src/execution.cr
+++ src/execution.cr
@@ -78,3 +78,24 @@ module Execution
     stdout.close
     stderr.close
   end
+
+  def run_cmd_silent(cmd : String, args : Array(String), output_file : Nil.class | String)
+    stdout = IO::Memory.new
+    stderr = IO::Memory.new
+    if typeof(output_file) == String
+      logfile = File.new("#{output_file}", "w")
+      status = Process.run(cmd, args: args, output: logfile, error: stderr)
+      if status.success?
+        # puts stage
+      else
+        puts stderr.colorize(:red)
+      end
+      logfile.close
+    else
+      status = Process.run(cmd, args: args, output: stdout, error: stderr)
+      # puts stage
+    end
+    stdout.close
+    stderr.close
+  end
+end
