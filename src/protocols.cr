18a099da6ea4aa53acaf6209640efe7dd213ac00
Francisco Adasme <francisco.adasme@gmail.com>
2023-11-27 18:25:27 -0300

    refactor: Rewrite SamplingProtocol#execute to avoid duplication

--- src/protocols.cr
+++ src/protocols.cr
@@ -111,183 +111,51 @@ module Protocols
     end
 
     def execute(lig : Ligand, parallel workers : Int? = nil, procs : Int = 4)
-      variants : Array(String) = [] of String
-      # #if @time_rmsd != 0 && @dimension == 1
-      # #  count = 0
-      # #  type = "rmsd"
-      # #  puts "Sampling protocol using RMSD".colorize(GREEN)
-      # #  rmsd_pairs.each.with_index do |pair, index|
-      # #    window = "w#{count += 1}"
-      # #    lw_rmsd = pair[0]
-      # #    up_rmsd = pair[1]
-      # #    # Writting namd configuration
-      # #    enhanced_sampling(lig.explicit_water, lig.basename, lig.topology_file, lig.coordinates_file, "#{type}.#{window}.namd", @time_rmsd, window, type, lig.output_frequency).to_s
-      # #    # Writting colvars configuration
-      # #    colvars(@metadynamics,
-      # #      lw_rmsd,
-      # #      up_rmsd,
-      # #      false,
-      # #      false,
-      # #      true,
-      # #      false,
-      # #      @wallconstant_force_rmsd,
-      # #      lig.pdb_reference,
-      # #      lig.lig_center.x,
-      # #      lig.lig_center.y,
-      # #      lig.lig_center.z,
-      # #      "#{type}.#{window}.colvars").to_s
-      # #    namd_exec = "namd2"
-      # #    # Arguments for GPU and CPU
-      # #    if lig.explicit_water
-      # #      arguments = ["#{type}.#{window}.namd", "+p", "4", "+devices", "0"]
-      # #    else
-      # #      arguments = ["#{type}.#{window}.namd", "+p", "4", "+setcpuaffinity"]
-      # #      # arguments = ["#{type}.#{window}.namd"]
-      # #    end
-      # #    puts "Runnning ABF on window '#{window}', with RMSD ranges from #{lw_rmsd} to #{up_rmsd}"
-      # #    # Namd execution
-      # #    run_namd(cmd = namd_exec, args = arguments, output_file = "#{type}.#{window}.out", stage = "abf", window = "#{window}")
-      # #    # Checking number of frames in every calculation.
-      # #    dcd_name = "outeabf.#{type}.#{window}.#{lig.basename}.dcd"
-      # #    if File.exists?(dcd_name)
-      # #      dcd = Path.new(dcd_name).expand.to_s
-      # #      puts "Done... #{n_frames(lig.pdb_system, dcd)} frames generated for window #{window}"
-      # #    else
-      # #      puts "No frames were generated in window 'w#{window}'"
-      # #    end
-      # #  end
-      # #end
-      case @colvars.size
-      when 1
-        type = @colvars[0].component.name
-        puts "Sampling protocol using #{type.underscore.gsub('_', ' ')}".colorize(GREEN)
-        count = -1
-        # Variants generation
-        # This block code add the variants strategy to start every window with
-        # a different random coordinate of the ligand using openbabel.
-        min_lastframe = Chem::Structure.from_pdb("min.lastframe.pdb")
-        time_per_variant = @simulation_time / @n_variants
+      desc = @colvars.join(" & ") { |cv| cv.component.name.underscore.gsub('_', ' ') }
+      type = @colvars.join('_') { |cv| cv.component.keyword.underscore }
+      puts "Sampling protocol using #{desc}".colorize(GREEN)
 
-        if @n_variants >= 2
-          puts "Creating variants: ".colorize(GREEN)
-          puts "Reference mol: #{lig.extended_mol}"
-          variants = create_variants(@n_variants, @threshold_rmsd_variants, lig.extended_mol)
+      variants = [] of String
+      if @n_variants >= 2
+        puts "Creating variants: ".colorize(GREEN)
+        puts "Reference mol: #{lig.extended_mol}"
+        variants = create_variants(@n_variants, @threshold_rmsd_variants, lig.extended_mol)
+      else
+        File.copy("min.lastframe.pdb", "v1.pdb")
+        variants << Path.new("v1.pdb").expand.to_s
+      end
+      time_per_variant = @simulation_time / @n_variants
+
+      combinations = Indexable
+        .cartesian_product(@colvars.map(&.window_colvars))
+        .cartesian_product(variants)
+      workers ||= Math.min(combinations.size, System.cpu_count) // procs
+      puts "Running #{combinations.size} MD runs in #{workers} parallel jobs with #{procs} cores each...".colorize(:blue)
+      combinations.each.with_index.concurrent_each(workers) do |(colvars, path), i|
+        window = "w#{i // variants.size + 1}"
+        variant = "v#{variants.index!(path) + 1}"
+        stem = "#{type}.#{window}.#{variant}"
+
+        structure = Chem::Structure.from_pdb(path)
+
+        enhanced_sampling(lig.explicit_water, lig.basename, lig.topology_file, lig.coordinates_file, "#{stem}.namd", time_per_variant, window, variant, type, lig.output_frequency)
+        colvars("#{stem}.colvars", colvars, structure, @metadynamics, @fullsamples)
+
+        print "Runnning ABF on window '#{window}', variant '#{variant}'."
+        colvars.each do |cv|
+          print " #{cv.component.name} ranges: #{cv.bounds}."
+        end
+        puts ".."
+        args = ["#{stem}.namd", "+p", procs.to_s]
+        args << "+devices" << "0" if lig.explicit_water
+        run_namd("namd2", args, "#{stem}.out", stage: "abf", window: window)
+
+        path = Path["outeabf.#{stem}.dcd"].expand
+        if File.exists?(path)
+          frames = n_frames(lig.pdb_system, path.to_s)
+          puts "Done. #{frames} frames generated for window #{window}, variant #{variant}"
         else
-          File.copy("min.lastframe.pdb", "v1.pdb")
-          variants.push(Path.new("v1.pdb").expand.to_s)
-        end
-
-        # # Minimization of all the variants before the sampling stage
-        # puts "Minimization of variants: ".colorize(GREEN)
-        # variants.each.with_index do |_, index|
-        #  variant = "v#{index += 1}"
-        #  minimize_variant(lig.explicit_water, "#{variant}.pdb", lig.topology_file)
-        # end
-        combinations = @colvars[0].window_colvars.cartesian_product(variants)
-        workers ||= Math.min(combinations.size, System.cpu_count) // procs
-        puts "Running #{combinations.size} MD runs in #{workers} parallel jobs with #{procs} cores each...".colorize(:blue)
-        combinations.each.with_index.concurrent_each(workers) do |(cv, variant_path), i|
-          window = "w#{i // variants.size + 1}"
-          index = variants.index! variant_path
-          variant = "v#{index + 1}"
-          # Writting namd configuration
-          # enhanced_sampling(lig.explicit_water, "min.#{variant}", lig.topology_file, lig.coordinates_file, "#{type}.#{window}.#{variant}.namd", time_per_variant, window, variant, type, lig.output_frequency).to_s
-          enhanced_sampling(lig.explicit_water, "#{lig.basename}", lig.topology_file, lig.coordinates_file, "#{type}.#{window}.#{variant}.namd", time_per_variant, window, variant, type, lig.output_frequency).to_s
-          # Writting colvars configuration
-          colvars(
-            "#{type}.#{window}.#{variant}.colvars",
-            [cv],
-            Chem::Structure.from_pdb(variant_path),
-            @metadynamics,
-            @fullsamples)
-          namd_exec = "namd2"
-          # Arguments for GPU and CPU
-          if lig.explicit_water
-            arguments = ["#{type}.#{window}.#{variant}.namd", "+p", procs.to_s, "+devices", "0"]
-          else
-            # arguments = ["#{type}.#{window}.#{variant}.namd", "+p", procs.to_s1, "+setcpuaffinity"]
-            arguments = ["#{type}.#{window}.#{variant}.namd", "+p", procs.to_s]
-            # arguments = ["#{type}.#{window}.namd"]
-          end
-          puts "Runnning ABF on window '#{window}', variant '#{variant}' with #{cv.component.name} ranges #{cv.bounds}"
-          # Namd execution
-          run_namd(cmd = namd_exec, args = arguments, output_file = "#{type}.#{window}.#{variant}.out", stage = "abf", window = "#{window}")
-          # Checking number of frames in every calculation.
-          dcd_name = "outeabf.#{type}.#{window}.#{variant}.dcd"
-          if File.exists?(dcd_name)
-            dcd = Path.new(dcd_name).expand.to_s
-            puts "Done... #{n_frames(lig.pdb_system, dcd)} frames generated for window #{window}, variant #{variant}"
-          else
-            puts "No frames were generated in window #{window}"
-          end
-        end
-      when 2
-        count = -1
-        type = "rmsd_rdgyr"
-        puts "Sampling protocol using RMSD".colorize(GREEN)
-        # Variants generation
-        # Variants
-        # This block code add the variants strategy to start every window with
-        # a different random coordinate of the ligand generated previously
-        # with openbabel.
-        # 10 initial variants will be generated, which will be the input for each window.
-        min_lastframe = Chem::Structure.from_pdb("min.lastframe.pdb")
-        time_per_variant = @simulation_time / @n_variants
-
-        if @n_variants >= 2
-          puts "Creating variants: ".colorize(GREEN)
-          variants = create_variants(@n_variants, @threshold_rmsd_variants, lig.extended_mol)
-        else
-          File.copy("min.lastframe.pdb", "v1.pdb")
-          variants.push(Path.new("v1.pdb").expand.to_s)
-        end
-        # # Minimization of all the variants before the sampling stage
-        # puts "Minimization of variants: ".colorize(GREEN)
-        # variants.each.with_index do |_, index|
-        #  variant = "v#{index += 1}"
-        #  minimize_variant(lig.explicit_water, "#{variant}.pdb", lig.topology_file)
-        # end
-        combinations = @colvars[0].window_colvars.cartesian_product(@colvars[1].window_colvars, variants)
-        workers ||= Math.min(combinations.size, System.cpu_count) // procs
-        combinations.each.with_index.concurrent_each(workers) do |(cv1, cv2, variant_path), i|
-          window = "w#{i // variants.size + 1}"
-          index = variants.index! variant_path
-          variant = "v#{index + 1}"
-          # Writting namd configuration
-          # enhanced_sampling(lig.explicit_water, "min.#{variant}", lig.topology_file, lig.coordinates_file, "#{type}.#{window}.#{variant}.namd", time_per_variant, window, variant, type, lig.output_frequency).to_s
-          enhanced_sampling(lig.explicit_water, lig.basename, lig.topology_file, lig.coordinates_file, "#{type}.#{window}.#{variant}.namd", time_per_variant, window, variant, type, lig.output_frequency).to_s
-          # Writting colvars configuration
-          # wallconstant_force for 2D must be fixed in the following function.
-          colvars(
-            "#{type}.#{window}.#{variant}.colvars",
-            [cv1, cv2],
-            Chem::Structure.from_pdb(variant_path),
-            @metadynamics,
-            @fullsamples)
-          namd_exec = "namd2"
-          # Arguments for GPU and CPU
-          if lig.explicit_water
-            arguments = ["#{type}.#{window}.#{variant}.namd", "+p", procs.to_s, "+devices", "0"]
-          else
-            # arguments = ["#{type}.#{window}.#{variant}.namd", "+p", procs.to_s, "+setcpuaffinity"]
-            arguments = ["#{type}.#{window}.#{variant}.namd", "+p", procs.to_s]
-            # arguments = ["#{type}.#{window}.namd"]
-          end
-          print "Runnning ABF on window '#{window}', variant '#{variant}'."
-          {cv1, cv2}.each do |cv|
-            print " #{cv.component.name} ranges: #{cv.bounds}."
-          end
-          puts
-          # Namd execution
-          run_namd(cmd = namd_exec, args = arguments, output_file = "#{type}.#{window}.out", stage = "abf", window = "#{window}")
-          # Checking number of frames in every calculation.
-          dcd_name = "outeabf.#{type}.#{window}.#{variant}.dcd"
-          if File.exists?(dcd_name)
-            dcd = Path.new(dcd_name).expand.to_s
-            puts "Done... #{n_frames(lig.pdb_system, dcd)} frames generated for window #{window}, variant #{variant}"
-          else
-            puts "No frames were generated in window '#{window}'"
-          end
+          puts "No frames were generated in window #{window}".colorize(:yellow)
         end
       end
     end
