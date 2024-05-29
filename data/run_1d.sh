55524e4264e6e6401b63ad968ba99df93c8993ab
Francisco Adasme <francisco.adasme@gmail.com>
2024-05-29 12:14:50 -0400

    Run example from any directory

--- data/run_1d.sh
+++ data/run_1d.sh
@@ -1,1 +1,6 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+cd $SCRIPT_DIR
 ../bin/moltiverse -l molecules.smi -o TEST_MOLECULE -n 250 -s 500 -f 1000 --bounds_colvars '0,0,0,0,0.0,4.0,8,10' --variants 1 --cores-per-run 1 --time 0.4 -n 10
