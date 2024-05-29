4b75b1c048a8dca8093c64519d3c74210c39abf8
Francisco Adasme <francisco.adasme@gmail.com>
2024-05-29 18:12:30 -0400

    Run xtb optimizations in parallel

--- src/moltiverse/prepare.cr
+++ src/moltiverse/prepare.cr
@@ -471,17 +471,27 @@ class Ligand
     t2 - t1
   end
 
-  def qm_refinement
+  def qm_refinement(parallel workers : Int = System.cpu_count)
     t1 = Time.monotonic
     puts "Performing QM refinement...".colorize(GREEN)
 
+    i = 0
+    cwd = Path[Dir.current]
     qm_refined_structures = [] of Chem::Structure
     Array(Chem::Structure)
       .from_sdf("#{@output_name}_mm.sdf")
-      .each do |structure|
-        next unless pdb = XTB.optimize(structure, cycles: 1500, level: :crude)
-        qm_refined_structures.push(pdb)
+      .concurrent_each(workers) do |structure|
+        i += 1
+        workdir = cwd / ("%05d" % i)
+        Dir.mkdir_p workdir
+        Dir.cd workdir
+        if pdb = XTB.optimize(structure, cycles: 1500, level: :crude)
+          qm_refined_structures.push(pdb)
+        end
+        Dir.cd cwd
+        Dir.delete workdir
       end
+    Dir.cd cwd
     qm_refined_structures.to_sdf "#{@output_name}_qm.sdf"
     qm_refined_structures.to_pdb "#{@output_name}_qm.pdb", bonds: :all
     puts "Output file: '#{@output_name}_qm.[sdf,pdb]'".colorize(TURQUOISE)
