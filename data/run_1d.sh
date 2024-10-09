19b86885ebcb753065789d4fcc80236b71a92e37
Mauricio Bedoya <maurobedoyat@gmail.com>
2024-10-09 00:47:42 -0300

    Delete run_1d.sh

--- data/run_1d.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-
-cd $SCRIPT_DIR
-../bin/moltiverse -l molecules.smi -o TEST_MOLECULE -p test -n 10
