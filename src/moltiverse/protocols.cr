77a3662cec644f1f55c4206db9fbf927c94e22d8
Mauricio Bedoya <maurobedoyat@gmail.com>
2025-03-16 02:16:06 -0300

    Remove variants option from protocols.cr

--- src/moltiverse/protocols.cr
+++ src/moltiverse/protocols.cr
@@ -5,7 +5,6 @@ class SamplingProtocol
   include YAML::Serializable
 
   getter colvars : Array(Colvar::Windowed)
-  getter n_variants : Int32
   getter metadynamics : Bool
   getter simulation_time : Float64
   getter fullsamples : Int32
@@ -31,7 +30,6 @@ class SamplingProtocol
     @hillweight : Float64,
     @hillwidth : Float64,
     @newhillfrequency : Int32,
-    @n_variants : Int32,
     @structure_generator : String = "cdpkit",
     @protonation_ph : Float64 = 7.0,
     @name : String = "default",
@@ -67,8 +65,6 @@ class SamplingProtocol
 
   def self.new(name : String) : self
     case name
-    when "c1"   then from_yaml {{read_file "#{__DIR__}/../../data/c1.yml"}}
-    when "test" then from_yaml {{read_file "#{__DIR__}/../../data/test.yml"}}
     else
       path = "#{name}.yml"
       found = File.exists?(path)
@@ -89,89 +85,51 @@ class SamplingProtocol
   end
 
   def describe
-    time_per_variant = @simulation_time / @n_variants
     total_time = @simulation_time * @colvars.product(&.windows)
-
-    puts "SAMPLING PROTOCOL using a #{@colvars.size}D collective variable".colorize(GREEN)
+    puts "SAMPLING PROTOCOL: #{@name}".colorize(GREEN)
+    puts "DESCRIPTION: #{@description}".colorize(GREEN) unless @description.empty?
+    puts "Using a #{@colvars.size}D collective variable".colorize(GREEN)
 
     @colvars.each do |cv|
       puts "#{cv.component.name}:"
       puts "Range of values:                    [ #{cv.bounds} ]"
       puts "Number of windows:                  [ #{cv.windows} ]"
-      puts "Number of variants:                 [ #{@n_variants} ]"
       puts "Width per window:                   [ #{cv.window_width} ]"
       puts "Wall force constant:                [ #{cv.force_constant} ]"
       puts "Simulation time:                    [ #{@simulation_time * cv.windows} ns ]"
     end
     puts
     puts "Simulation time per window:         [ #{@simulation_time} ns ]"
-    puts "Simulation time per variant:        [ #{time_per_variant} ns ]"
     puts "Total simulation time:              [ #{total_time} ns ]"
     puts "Sampling method:                    [ #{@metadynamics ? "M-eABF" : "eABF"} ]"
-  end
-
-  def create_variants(mol_ref : String, samples : Int = 500) : Array(Chem::Structure)
-    variants = [] of Chem::Structure
-    (0...samples).concurrent_each(workers: System.cpu_count) do
-      variants << rand_conf(mol_ref)
-    end
-
-    dism = HClust::DistanceMatrix.new(variants.size) do |i, j|
-      variants[i].pos.rmsd variants[j].pos, minimize: true
-    end
-    dendrogram = HClust.linkage(dism, :single)
-    dendrogram.flatten(count: @n_variants).map do |idxs|
-      variants[idxs[dism[idxs].centroid]]
-    end
   end
 
   def execute(lig : Ligand, cpus : Int = System.cpu_count)
     desc = @colvars.join(" & ") { |cv| cv.component.name.underscore.gsub('_', ' ') }
     type = @colvars.join('_') { |cv| cv.component.keyword.underscore }
     puts "Sampling protocol using #{desc}".colorize(GREEN)
-
-    variants = [] of String
-    if @n_variants >= 2
-      puts "Creating #{@n_variants} variants: ".colorize(GREEN)
-      puts "Reference mol: #{lig.extended_mol}"
-      create_variants(lig.extended_mol).each_with_index(offset: 1) do |variant, i|
-        path = Path["v#{i}.pdb"].expand
-        variant.to_pdb path, bonds: :none
-        variants << path.to_s
-      end
-    else
-      File.copy("min.lastframe.pdb", "v1.pdb")
-      variants << Path.new("v1.pdb").expand.to_s
-    end
-
-    combinations = Indexable
-      .cartesian_product(@colvars.map(&.window_colvars))
-      .cartesian_product(variants)
-    cpus_per_run = 2
-    workers = Math.min(combinations.size, cpus // cpus_per_run)
-    puts "Running #{combinations.size} MD runs in #{workers} parallel jobs with #{cpus_per_run} cores each...".colorize(:blue)
-    combinations.concurrent_each(workers) do |(colvars, path), i|
-      window = "w#{i // variants.size + 1}"
-      variant = "v#{variants.index!(path) + 1}"
-      stem = "#{type}.#{window}.#{variant}"
-
-      structure = Chem::Structure.from_pdb(path)
-
-      NAMD::Input.enhanced_sampling("#{stem}.namd", lig, @simulation_time / @n_variants, @output_frequency)
-      NAMD::Input.colvars("#{stem}.colvars", colvars, structure, @metadynamics, @fullsamples, @hillweight, @hillwidth, @newhillfrequency)
-
-      print "Runnning ABF on window '#{window}', variant '#{variant}'."
-      colvars.each do |cv|
-        print " #{cv.component.name} ranges: #{cv.bounds}."
-      end
+  
+    # Use minimized structure directly
+    structure = Chem::Structure.from_pdb("min.lastframe.pdb")
+  
+    # Process each window from the collective variables
+    @colvars.first.window_colvars.each_with_index do |colvar, i|
+      window = "w#{i + 1}"
+      stem = "#{type}.#{window}"
+  
+      NAMD::Input.enhanced_sampling("#{stem}.namd", lig, @simulation_time, @output_frequency)
+      NAMD::Input.colvars("#{stem}.colvars", [colvar], structure, @metadynamics, @fullsamples, @hillweight, @hillwidth, @newhillfrequency)
+  
+      print "Running ABF on window '#{window}'."
+      print " #{colvar.component.name} ranges: #{colvar.bounds}."
       puts ".."
-      NAMD.run "#{stem}.namd", cores: cpus_per_run, retries: 5
-
+      
+      NAMD.run "#{stem}.namd", cores: cpus, retries: 5
+  
       if File.exists?("outeabf.#{stem}.dcd")
         path = Path["outeabf.#{stem}.dcd"].expand
-        # if File.exists?(path)
         frames = n_frames(path.to_s)
-        puts "Done. #{frames} frames generated for window #{window}, variant #{variant}"
+        puts "Done. #{frames} frames generated for window #{window}"
       else
         puts "No frames were generated in window #{window}".colorize(:yellow)
       end
